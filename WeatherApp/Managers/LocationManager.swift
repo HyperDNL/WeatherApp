eec49af9ffaf2c7d5bd2dcc4fe9da23b5df1ee45
Daniel López <hyperdaniel579exe@gmail.com>
2023-04-25 17:46:44 -0600

    Update LocationManager.swift

--- WeatherApp/Managers/LocationManager.swift
+++ WeatherApp/Managers/LocationManager.swift
@@ -15,7 +15,7 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
         manager.delegate = self
     }
     
-    // Solicita la entrega por única vez de la ubicación actual del usuario,
+    // Solicita la entrega por única vez de la ubicación actual del usuario
     func requestLocation() {
         isLoading = true
         manager.requestLocation()
